e22cc8b9f866f382df8b020690c586972cf63e4c
pietwoldinga <pieterwoldinga@gmail.com>
2022-04-18 16:18:51 -0400

    Initial commit of Kali networking Python programming

---
+++ scan.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+from lxml import etree
+from subprocess import Popen
+import argparse
+import os
+
+def get_ip(machine_name) :
+    pass
+
+class Scanner :
+    def __int__(self) :
+        pass
+
+if __name__ == '__main__' :
+    scan = Scanner()
+    print('Hello')
---
+++ tcp_client_loopback.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+import socket
+
+target_host = "127.0.0.1"
+target_port = 9998
+
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the client
+client.connect((target_host, target_port))
+
+# send some data
+client.send(b"Hello there!\r\n\r\n")
+
+# recieve some data
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
